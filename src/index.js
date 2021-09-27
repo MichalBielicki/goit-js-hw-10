40b2a627561c1040bb201b2961f72abe1ed24b03
MichalBielicki <80898561+MichalBielicki@users.noreply.github.com>
2021-09-27 11:35:39 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import fetchCountries from './fetchCountries.js';
+import fetchCountries from './js/fetchCountries.js';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 
